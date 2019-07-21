618c2afc23b06c70fa78717dbb7b3b5e4a9985c4
wanghaichao <wanghaichao@aiyunxiao.com>
2019-07-21 20:27:11 +0800

    管理员注册问题

--- workplan/myplan/admin.py
+++ workplan/myplan/admin.py
@@ -3,16 +3,17 @@ from django.contrib import admin
 # Register your models here.
 from myplan.models import QA,Group,PlanType,Plan
 
-admin.site.register(QA)
+# admin.site.register(QA)
 # admin.site.register(Group)
 admin.site.register(PlanType)
 admin.site.register(Plan)
 
-# class QAAdmin(admin.ModelAdmin):
-#     # fields = ('username', 'password')
-#     list_display = ("qaName","qapwd","userGroup")
+@admin.register(QA)
+class QAAdmin(admin.ModelAdmin):
+    # fields = ('username', 'password')
+    list_display = ("qaName","qapwd","qaGroup","addTime")
 
 @admin.register(Group)
 class GroupAdmin(admin.ModelAdmin):
     # fields = ('username', 'password')
-    list_display = ("groupId","groupName","addTime")
+    list_display = ("groupName","addTime")

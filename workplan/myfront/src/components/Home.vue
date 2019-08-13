8773989f2c36d8fc6e6817f66032517029758061
wanghaichao <wanghaichao@aiyunxiao.com>
2019-08-13 20:31:58 +0800

    添加前端

--- workplan/myfront/src/components/Home.vue
+++ workplan/myfront/src/components/Home.vue
@@ -8,19 +8,19 @@
     <el-table-column prop="name" label="姓名" width="120"></el-table-column>
 
     <el-table-column :label=data.time v-for=" data in tableData" :key="key">
-      <el-table-column prop="planname" label="上午" width="120">
-        <template slot-scope="scope" >
-          {{scope.row.planname.split("@@@")[0]}}
+      <el-table-column prop="planlist" label="上午" width="120">
+        <template slot-scope="scope"  v-for="i in (0,1)">
+          {{scope.row.planlist[0].planname.split("@@@")[0]}}
           <!--{{tableData[scope.$index][i]}}-->
           <!--{{tableData[scope.$index][i].planname.split("@@@")[0]}}-->
         </template>
       </el-table-column>
       <el-table-column
-        prop="planname"
+        prop="planlist"
         label="下午"
         width="120">
-        <template slot-scope="scope">
-          {{scope.row.planname.split("@@@")[1]}}
+        <template slot-scope="scope" >
+          {{scope.row.planlist[0].planname.split("@@@")[1]}}
           <!--{{tableData[scope.$index][i]}}-->
           <!--{{tableData[scope.$index][i].row.planname.split("@@@")[1]}}-->
         </template>
@@ -40,8 +40,9 @@
           {
             group: '错题本',
             name: '王小虎',
-            list:[{date:"2018-09-08",planname:"测试1@@@测试2"},{date:"2018-09-08",planname:"测试3@@@测试4"}]
-            // time: "2018-09-08",
+            // planlist:[{date:"2018-09-08",planname:"测试1@@@测试2"},{date:"2018-09-09",planname:"测试3@@@测试4"}],
+            planlist:[{date:"2018-09-08",planname:"测试1@@@测试2"}],
+            time: "2018-09-08",
             // planname: "测试1@@@测试2"
           },
           // {
